21faa4bc3459d3cd7179ee51424fddc2a00c4f4a
shira <m.shira2009@gmail.com>
2020-10-13 13:33:23 +0300

    Updates

---
+++ precache-manifest.251f17d48969eab091c5276fbc51163d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683a233c76a1054226aa77feaf96aac7",
+    "url": "/repsudoku/index.html"
+  },
+  {
+    "revision": "3f20aa42a8c7641c0513",
+    "url": "/repsudoku/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "568fbeffc8b4e4a0b2ca",
+    "url": "/repsudoku/static/css/main.d173a917.chunk.css"
+  },
+  {
+    "revision": "3f20aa42a8c7641c0513",
+    "url": "/repsudoku/static/js/2.685ed6c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/repsudoku/static/js/2.685ed6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568fbeffc8b4e4a0b2ca",
+    "url": "/repsudoku/static/js/main.5521fb75.chunk.js"
+  },
+  {
+    "revision": "1125a77dd0fc59109448",
+    "url": "/repsudoku/static/js/runtime-main.2920056d.js"
+  },
+  {
+    "revision": "2497ca2981cd6fe17376b19d5fb41687",
+    "url": "/repsudoku/static/media/background.2497ca29.jpg"
+  },
+  {
+    "revision": "08a83089bd046b11b99b287fcb5cf19a",
+    "url": "/repsudoku/static/media/crying.08a83089.gif"
+  },
+  {
+    "revision": "87c4538b3b5e5c2bd195bc0879e1f36f",
+    "url": "/repsudoku/static/media/good.87c4538b.gif"
+  },
+  {
+    "revision": "e175412e95b9aeba27a71b47937e0fad",
+    "url": "/repsudoku/static/media/sound.e175412e.mp3"
+  },
+  {
+    "revision": "5d67370ee8ff1a25b37f0dfc3706f344",
+    "url": "/repsudoku/static/media/star.5d67370e.gif"
+  },
+  {
+    "revision": "05d5cb191ab4f0092bd6a120f8cd828e",
+    "url": "/repsudoku/static/media/title2.05d5cb19.png"
+  },
+  {
+    "revision": "6e6e0cd3776298a725fe1b6414ed932f",
+    "url": "/repsudoku/static/media/welcome.6e6e0cd3.gif"
+  }
+]);
